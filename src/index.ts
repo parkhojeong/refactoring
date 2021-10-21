71a928654d047791abe19da962a4fb64ea34c972
HojeongPark <parkhj062@gmail.com>
2021-10-22 01:06:54 +0900

    volumeCredits 계산 로직 별도로 분리

--- src/index.ts
+++ src/index.ts
@@ -7,22 +7,23 @@ function statement(invoice, plays) {
     let result = `청구 내역 (고객명 ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-
         result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
 
-    result += `총액 : ${usd(totalAmount )}\n`;
+    result += `총액 : ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 
-    function usd(aNumber){
+    function usd(aNumber) {
         return new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD",
             minimumFractionDigits: 2
-        }).format(aNumber/ 100);
+        }).format(aNumber / 100);
     }
 
     function volumeCreditsFor(aPerformance) {
