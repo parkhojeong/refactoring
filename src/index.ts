953f6b6c556e067b6c48a05b76f07fff1ed8b398
HojeongPark <parkhj062@gmail.com>
2021-10-22 01:11:32 +0900

    totalAmount 반복문 쪼개기

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import invoices from "./invoices";
 import plays from "./plays";
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
-
     let result = `청구 내역 (고객명 ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
         result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    }
+
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
         totalAmount += amountFor(perf);
     }
 
