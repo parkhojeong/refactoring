78757651784923cf3c7704e8f58dba0c3df2432b
HojeongPark <parkhj062@gmail.com>
2021-10-22 01:00:54 +0900

    volumeCreditsFor 함수로 추출

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,7 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits += volumeCreditsFor(perf);
 
         result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
@@ -22,6 +21,14 @@ function statement(invoice, plays) {
     result += `총액 : ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
+
+    function volumeCreditsFor(perf) {
+        let volumeCredits = 0
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+
+        return volumeCredits
+    }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playId]
