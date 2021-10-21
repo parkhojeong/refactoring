e0cf87128231ecdd6c09df29e32646086285d7c2
HojeongPark <parkhj062@gmail.com>
2021-10-22 00:45:34 +0900

    refactor init

---
+++ src/invoices.ts
@@ -0,0 +1,19 @@
+export default [
+  {
+    "customer": "Bigco",
+    "performances": [
+      {
+        "playId": "hamlet",
+        "audience": 55
+      },
+      {
+        "playId": "as-like",
+        "audience": 35
+      },
+      {
+        "playId": "othello",
+        "audience": 40
+      }
+    ]
+  }
+]
---
+++ src/plays.ts
@@ -0,0 +1,14 @@
+export default {
+  "hamlet": {
+    "name": "Hamlet",
+    "type": "tragedy"
+  },
+  "as-like": {
+    "name": "As YOU LIKE IT",
+    "type": "comedy"
+  },
+  "othello": {
+    "name": "Othello",
+    "type": "tragedy"
+  }
+}
