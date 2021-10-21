909f00780342939dac20c927d28a2443b1b27215
HojeongPark <parkhj062@gmail.com>
2021-10-22 00:50:20 +0900

    우변 함수로 추출

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playId];
+        const play = playFor(perf)
         let thisAmount = amountFor(perf, play)
 
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -25,24 +25,30 @@ function statement(invoice, plays) {
     result += `총액 : ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
+
+    function playFor(aPerformance){
+        return plays[aPerformance.playId]
+    }
 }
 
-function amountFor(perf, play) {
+
+
+function amountFor(aPerformance, play) {
     let result = 0;
 
     switch (play.type) {
         case 'tragedy':
             result = 40000;
-            if (perf.audience > 30) {
-                result += 1000 * (perf.audience - 30);
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":
             result = 30000;
-            if (perf.audience > 20) {
-                result += 10000 + 500 * (perf.audience - 20);
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            result += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
             throw new Error(`알수없는장르: ${play.type}`)
