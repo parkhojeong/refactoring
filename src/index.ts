a8c28d6d76e068783dfc5cb3babe638bd2375927
HojeongPark <parkhj062@gmail.com>
2021-10-22 00:53:01 +0900

    amountFor 함수 위치 statement 안으로 이동

--- src/index.ts
+++ src/index.ts
@@ -25,35 +25,34 @@ function statement(invoice, plays) {
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 
-    function playFor(aPerformance){
+    function playFor(aPerformance) {
         return plays[aPerformance.playId]
     }
-}
 
+    function amountFor(aPerformance, play) {
+        let result = 0;
 
+        switch (play.type) {
+            case 'tragedy':
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알수없는장르: ${play.type}`)
+        }
 
-function amountFor(aPerformance, play) {
-    let result = 0;
-
-    switch (play.type) {
-        case 'tragedy':
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`알수없는장르: ${play.type}`)
+        return result;
     }
-
-    return result;
 }
+
 
 console.log(statement(invoices[0], plays));
