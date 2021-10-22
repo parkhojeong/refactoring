1444dd33350ed5373dfd5684fcbd245d7602eba9
HojeongPark <parkhj062@gmail.com>
2021-10-22 22:08:25 +0900

    reduce() 적용

--- src/index.ts
+++ src/index.ts
@@ -25,19 +25,11 @@ function statement(invoice, plays) {
     }
 
     function totalAmount(data) {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
+        return data.performances.reduce((total, p)=> total + p.amount , 0);
     }
 
     function totalVolumeCredits(data) {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.volumneCredits;
-        }
-        return result;
+        return data.performances.reduce((total, p)=> total + p.volumneCredits , 0);
     }
 
     function volumeCreditsFor(aPerformance) {
