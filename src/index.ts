299d62a20621df98bdf4cd76f23c00efb1476fd2
HojeongPark <parkhj062@gmail.com>
2021-10-22 21:59:35 +0900

    playFor 제거

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ function statement(invoice, plays) {
         let result = `청구 내역 (고객명 ${data.customer}\n`;
 
         for (let perf of data.performances) {
-            result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+            result += `${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         }
 
         result += `총액 : ${usd(totalAmount())}\n`;
@@ -50,7 +50,7 @@ function statement(invoice, plays) {
         function volumeCreditsFor(aPerformance) {
             let result = 0
             result += Math.max(aPerformance.audience - 30, 0);
-            if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+            if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
 
             return result
         }
@@ -58,7 +58,7 @@ function statement(invoice, plays) {
         function amountFor(aPerformance) {
             let result = 0;
 
-            switch (playFor(aPerformance).type) {
+            switch (aPerformance.play.type) {
                 case 'tragedy':
                     result = 40000;
                     if (aPerformance.audience > 30) {
@@ -73,7 +73,7 @@ function statement(invoice, plays) {
                     result += 300 * aPerformance.audience;
                     break;
                 default:
-                    throw new Error(`알수없는장르: ${playFor(aPerformance).type}`)
+                    throw new Error(`알수없는장르: ${aPerformance.play.type}`)
             }
 
             return result;
