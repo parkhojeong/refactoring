ae32dc65f506d60dd247a25d4f555b6554c9db25
HojeongPark <parkhj062@gmail.com>
2021-10-22 22:01:53 +0900

    volumeCreditsFor() 제거

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,20 @@ function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumneCredits = volumeCreditsFor(result);
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playId]
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+
+        return result
     }
 
     function amountFor(aPerformance) {
@@ -66,18 +75,12 @@ function statement(invoice, plays) {
         function totalVolumeCredits() {
             let result = 0;
             for (let perf of data.performances) {
-                result += volumeCreditsFor(perf);
+                result += perf.volumneCredits;
             }
             return result;
         }
 
-        function volumeCreditsFor(aPerformance) {
-            let result = 0
-            result += Math.max(aPerformance.audience - 30, 0);
-            if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
 
-            return result
-        }
 
 
     }
